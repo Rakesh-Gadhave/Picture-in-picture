dcca8920a78023e088ba9d5ebc0eeb1d9daa02e0
Rakesh-Gadhave <gadhaverakesh1996@gmail.com>
2024-06-15 17:03:07 +0530

    picture in picture

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream,passs to video element, then play
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    }catch (error){
+        //Catch Error Here
+        console.log(error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    //Disable Button
+    button.disable = true;
+    //start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    //Reset Button
+    button.disabled = false
+});
+
+//on load
+selectMediaStream();
